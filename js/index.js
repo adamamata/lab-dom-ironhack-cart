c67afe08392d09e9aa77a0d43496f720d83ab895
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-25 16:41:47 +0200

    almost done

--- js/index.js
+++ js/index.js
@@ -1,42 +1,46 @@
 // ITERATION 1
-
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  const total = price * quantity; 
+  const subTotal = product.querySelector('.subtotal span');
+  return subTotal.innerHTML = total;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName('product');
+  let sum = 0;
+  for (let i = 0; i < products.length; i++){
+    sum += updateSubtotal(products[i]);
+  }
+
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerHTML = sum;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  const targetRemove = target.parentNode.parentNode;
+  const parentOfTarget = targetRemove.parentNode;
+  parentOfTarget.removeChild(targetRemove);
+  calculateAll();
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  const target = event.currentTarget;
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const removeButton = document.querySelectorAll('.btn-remove');
+  for (let i = 0; i < removeButton.length; i++){
+    removeButton[i].addEventListener('click', removeProduct);
+  }
+  const createProductButton = document.querySelector('.create-product');
+  createProductButton.addEventListener('click', createProduct); 
 });
